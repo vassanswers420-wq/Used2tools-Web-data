38a756e49ea8c129d54e247ca3290d6da124d038
vassanswers420-wq <vassanswers420@gmail.com>
2026-03-06 17:00:37 +0530

    Update update_prices.py

--- update_prices.py
+++ update_prices.py
@@ -4,27 +4,52 @@ from datetime import datetime
 import os
 
 API_KEY = os.getenv("TRADEWATCH_KEY")
-
 headers = {"api-key": API_KEY}
 
+# -------------------
+# URLs
+# -------------------
 crypto_url = "https://api.tradewatch.io/crypto/quotes?symbols=BTCUSD,XRPUSD"
+
 commodities_url = "https://api.tradewatch.io/commodities/quotes?symbols=COPPER,GOLD,SILVER,NICKEL,ALUMINIUM"
 
+currency_url = "https://api.tradewatch.io/quotes?symbols=AUDUSD,CNYUSD,USDINR"
+
+# -------------------
+# Main Prices Object
+# -------------------
 prices = {}
 
-# crypto
+# -------------------
+# CRYPTO
+# -------------------
 r = requests.get(crypto_url, headers=headers).json()["items"]
+
 for c in r:
     if c["symbol"] == "BTCUSD":
         prices["btc"] = c["mid"]
     if c["symbol"] == "XRPUSD":
         prices["xrp"] = c["mid"]
 
-# commodities
+# -------------------
+# COMMODITIES
+# -------------------
 r = requests.get(commodities_url, headers=headers).json()["items"]
+
 for c in r:
     prices[c["symbol"].lower()] = c["mid"]
 
+# -------------------
+# CURRENCIES
+# -------------------
+r = requests.get(currency_url, headers=headers).json()["items"]
+
+for c in r:
+    prices[c["symbol"].lower()] = c["mid"]
+
+# -------------------
+# FINAL JSON
+# -------------------
 data = {
     "updated": datetime.utcnow().isoformat(),
     "assets": prices
